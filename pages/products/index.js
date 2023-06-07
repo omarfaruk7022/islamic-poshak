c339645468df45868e8ef794651d3de0e4879414
omarfaruk7022 <farhanfaruk221@gmail.com>
2023-06-07 15:25:19 +0600

    error solved

--- pages/products/index.js
+++ pages/products/index.js
@@ -1,40 +1,31 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import ProductsCard from "../../Components/Common/ProductsCard";
 import { useQuery } from "@tanstack/react-query";
 import NavbarOther from "@/Components/Common/NavbarOther";
 import Loading from "@/Components/Common/Loading";
 
-
-export default function Products(props) {
-  const { isLoading, error, data } = useQuery({
-    queryKey: ["products"],
-    queryFn: () =>
-      fetch("http://localhost:5000/api/product").then((res) => res.json()),
-    initialData: props.products,
-  });
+export async function loadPosts() {
+  const res = await fetch("http://localhost:5000/api/product");
+  const products = await res.json();
+  return products;
+}
+export default function Products(products) {
   return (
     <div>
       <NavbarOther />
       <div className="grid grid-cols-1 gap-3 px-4 md:grid-cols-2 lg:grid-cols-4">
-        {isLoading && data?.data === undefined ? (
-          <Loading />
-        ) : error ? (
-          <h1>{error}</h1>
-        ) : (
-          data?.data.map((product) => (
-            <ProductsCard key={product.id} product={product} />
-          ))
-        )}
+        {products?.products?.data.map((product) => (
+          <ProductsCard key={product.id} product={product} />
+        ))}
       </div>
     </div>
   );
 }
-export async function getStaticProps () {
-  const res = await fetch("http://localhost:5000/api/product");
-  const products = await res.json();
+export async function getStaticProps() {
+  const products = await loadPosts();
   return {
     props: {
       products,
     },
   };
-};
+}
