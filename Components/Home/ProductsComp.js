7cd44ebc1154da58f3a3aa4eb6694277f7d870ce
omarfaruk7022 <farhanfaruk221@gmail.com>
2023-06-07 17:50:40 +0600

    error solved

--- Components/Home/ProductsComp.js
+++ Components/Home/ProductsComp.js
@@ -4,18 +4,11 @@ import { useQuery } from "@tanstack/react-query";
 import Loading from "../Common/Loading";
 
 export default function ProductsComp() {
-  // const [products,setProducts] = useState()
   const { isLoading, error, data } = useQuery({
     queryKey: ["products"],
     queryFn: () =>
       fetch("http://localhost:5000/api/product").then((res) => res.json()),
   });
-  //   useEffect(() => {
-  //     fetch("http://localhost:5000/api/product")
-  //     .then(res => res.json())
-  //     .then(data => setProducts(data))
-  //   },[])
-
   return (
     <div className="grid grid-cols-1 gap-3 px-4 md:grid-cols-2 lg:grid-cols-4">
       {isLoading ? (
--- pages/products/index.js
+++ pages/products/index.js
@@ -1,24 +1,32 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ProductsCard from "../../Components/Common/ProductsCard";
 import NavbarOther from "@/Components/Common/NavbarOther";
 import { loadProducts } from "@/lib/load-products";
 
-export async function getStaticProps() {
-  const products = await loadProducts();
-  
-  return {
-    props: {
-      products,
-    },
-  };
-}
-export default function Products(products) {
-  console.log(products?.products?.data)
+// export async function getStaticProps() {
+//   const products = await loadProducts();
+
+//   return {
+//     props: {
+//       products,
+//     },
+//   };
+// }
+
+export default function Products() {
+  const [products, setProducts] = useState();
+  useEffect(() => {
+    fetch("http://localhost:5000/api/product")
+      .then((res) => res.json())
+      .then((data) => setProducts(data));
+  }, []);
+
+  console.log(products);
   return (
     <div>
       <NavbarOther />
       <div className="grid grid-cols-1 gap-3 px-4 md:grid-cols-2 lg:grid-cols-4">
-        {products?.products?.data.map((product) => (
+        {products?.data.map((product) => (
           <ProductsCard key={product.id} product={product} />
         ))}
       </div>
