ded85db0c569a2d6d75ce6e4cdcb0511f73918fb
omarfaruk7022 <farhanfaruk221@gmail.com>
2023-07-25 13:53:55 +0600

    minor change

--- Components/Home/Banner.js
+++ Components/Home/Banner.js
@@ -24,10 +24,10 @@ export default function Banner() {
           pagination={{
             clickable: true,
           }}
-          // autoplay={{
-          //   delay: 2500,
-          //   disableOnInteraction: false,
-          // }}
+          autoplay={{
+            delay: 2500,
+            disableOnInteraction: false,
+          }}
           modules={[Pagination, Navigation]}
         >
           <SwiperSlide>
