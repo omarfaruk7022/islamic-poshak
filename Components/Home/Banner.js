0c88e9bbd16d277004a027ec041620014b6a9960
omarfaruk7022 <farhanfaruk221@gmail.com>
2023-07-25 13:59:22 +0600

    minor change

--- Components/Home/Banner.js
+++ Components/Home/Banner.js
@@ -11,7 +11,6 @@ import banner3 from "../../assets/images/Banner3.jpg";
 import banner4 from "../../assets/images/Banner4.jpg";
 import banner5 from "../../assets/images/Banner5.jpg";
 import logo from "../../assets/images/logo.png";
-// import req3ired modules
 
 export default function Banner() {
   return (
@@ -80,38 +79,6 @@ export default function Banner() {
             </div>
           </SwiperSlide>
         </Swiper>
-        {/* <section className="bg-[url(https://i.ibb.co/w0WFXtx/Banner1.jpg)] bg-cover bg-center bg-no-repeat">
-          <div className=" mx-auto max-w-screen-xl px-4 py-32 sm:px-6 lg:flex lg:h-screen lg:items-center lg:px-8">
-            <div className="max-w-xl ltr:sm:text-left rtl:sm:text-right">
-              <h1 className="text-3xl font-extrabold sm:text-5xl">
-                Let us find your
-                <strong className="block font-extrabold text-rose-700">
-                  Forever Home.
-                </strong>
-              </h1>
-              <p className="mt-4 max-w-lg sm:text-xl/relaxed">
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-                Nesciunt illo tenetur fuga ducimus numquam ea!
-              </p>
-
-              <div className="mt-8 flex flex-wrap gap-4 text-center">
-                <a
-                  href="#"
-                  className="block w-full rounded bg-rose-600 px-12 py-3 text-sm font-medium text-white shadow hover:bg-rose-700 focus:outline-none focus:ring active:bg-rose-500 sm:w-auto"
-                >
-                  Get Started
-                </a>
-
-                <a
-                  href="#"
-                  className="block w-full rounded bg-white px-12 py-3 text-sm font-medium text-rose-600 shadow hover:text-rose-700 focus:outline-none focus:ring active:text-rose-500 sm:w-auto"
-                >
-                  Learn More
-                </a>
-              </div>
-            </div>
-          </div>
-        </section> */}
       </div>
     </div>
   );
