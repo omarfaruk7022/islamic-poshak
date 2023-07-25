36006a4b6d3584a87853ad4315546d60087158e4
omarfaruk7022 <farhanfaruk221@gmail.com>
2023-07-25 14:08:49 +0600

    minor change

--- Components/Home/Banner.js
+++ Components/Home/Banner.js
@@ -11,7 +11,6 @@ import banner4 from "../../assets/images/banner4.jpg";
 import banner5 from "../../assets/images/banner5.jpg";
 import logo from "../../assets/images/logo.png";
 
-
 export default function Banner() {
   return (
     <div>
