b9697157c43cb8c6e442f1b9e8d263a90952b505
omarfaruk7022 <farhanfaruk221@gmail.com>
2023-08-10 12:39:10 +0600

    spinner design fixed

--- Components/Common/Loading.js
+++ Components/Common/Loading.js
@@ -4,7 +4,7 @@ import { ProgressSpinner } from "primereact/progressspinner";
 export default function Loading() {
   return (
     <div>
-      <div className="flex justify-center items-center ">
+      <div className="flex justify-center items-center">
         <ProgressSpinner />
       </div>
     </div>
